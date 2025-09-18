e08ce52c5c809e4f27b5548d7946e8d6dcaa8a2c
Himanshukr008 <hk1996588@gmail.com>
2025-09-18 18:15:48 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaSignOutAlt } from 'react-icons/fa';
+import './Navbar.css';
+import './diet'
+import './challenges'
+import './motivation'
+
+const Navbar = ({ username, onLogout }) => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        Fit<span className="navbar-highlight">Quest</span>
+      </div>
+      <div class="navigate">
+        <a href="#Diet">Diet Chart</a>
+        <a href="#Challenges">Challenges</a>
+        <a href="#Motivation">Motivation</a>
+      </div>
+      <div className="navbar-user">
+        <span>Welcome, {username}</span>
+        <button onClick={onLogout} className="logout-button">
+          <FaSignOutAlt />
+          <span className="logout-text">Logout</span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
